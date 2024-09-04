42e218b1f8a49d0b9515793e67c8048402f7d97b
tae.y <0211ilyoil@gmail.com>
2024-09-04 17:02:18 +0900

    setting :: project setting

---
+++ src/test/java/spring/Java_MyBatis/JavaMyBatisApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.Java_MyBatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMyBatisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
