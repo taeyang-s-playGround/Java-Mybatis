f588445746d85ac1d836ff4bfb8e6167a82a32bc
tae.y <0211ilyoil@gmail.com>
2024-09-17 21:38:24 +0900

    feat :: mybatis 설정

--- src/main/java/spring/Java_MyBatis/JavaMyBatisApplication.java
+++ src/main/java/spring/Java_MyBatis/JavaMyBatisApplication.java
@@ -2,8 +2,11 @@ package spring.Java_MyBatis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import spring.Java_MyBatis.domain.config.MyBatisConfig;
 
 @SpringBootApplication
+@Import(MyBatisConfig.class)
 public class JavaMyBatisApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/spring/Java_MyBatis/domain/config/MyBatisConfig.java
@@ -0,0 +1,22 @@
+package spring.Java_MyBatis.domain.config;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+@MapperScan("com.example.mybatis.Mapper")
+public class MyBatisConfig {
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource);
+        sessionFactory.setTypeAliasesPackage("com.example.mybatis.Entity"); // Entity 클래스가 있는 패키지 설정
+        return sessionFactory.getObject();
+    }
+
+}
