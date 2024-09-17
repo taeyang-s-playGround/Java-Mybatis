e8efcb0d9dd3c5fdbcb1fb894fcf9b1750cd2a2e
tae.y <0211ilyoil@gmail.com>
2024-09-17 21:43:00 +0900

    feat ::  user 엔티티 생성

---
+++ src/main/java/spring/Java_MyBatis/domain/entity/User.java
@@ -0,0 +1,13 @@
+package spring.Java_MyBatis.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+public class User {
+
+    private Long id;
+    private String username;
+    private String accountId;
+}
