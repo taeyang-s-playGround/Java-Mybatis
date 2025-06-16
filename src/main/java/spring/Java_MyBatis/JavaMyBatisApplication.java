e5a148bcc8f82dc353f57538059eb6bdf4760e38
tae.y <0211ilyoil@gmail.com>
2025-06-16 13:45:10 +0900

    fix :: mapper 빈 등록 버그

--- src/main/java/spring/Java_MyBatis/JavaMyBatisApplication.java
+++ src/main/java/spring/Java_MyBatis/JavaMyBatisApplication.java
@@ -1,12 +1,14 @@
 package spring.Java_MyBatis;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 import spring.Java_MyBatis.domain.config.MyBatisConfig;
 
 @SpringBootApplication
-@Import(MyBatisConfig.class)
+@MapperScan("spring.Java_MyBatis.domain.mapper")
+@Import(MyBatisConfig.clas)
 public class JavaMyBatisApplication {
 
 	public static void main(String[] args) {
