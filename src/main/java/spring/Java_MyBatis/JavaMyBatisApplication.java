a906f3f2d7466b98e4a63feae7cb83421c719dc2
tae.y <0211ilyoil@gmail.com>
2025-07-04 17:16:02 +0900

    ref :: 설정 수정

--- src/main/java/spring/Java_MyBatis/JavaMyBatisApplication.java
+++ src/main/java/spring/Java_MyBatis/JavaMyBatisApplication.java
@@ -3,12 +3,8 @@ package spring.Java_MyBatis;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
-import spring.Java_MyBatis.domain.config.MyBatisConfig;
 
 @SpringBootApplication
-@MapperScan("spring.Java_MyBatis.domain.mapper")
-@Import(MyBatisConfig.class)
 public class JavaMyBatisApplication {
 
 	public static void main(String[] args) {
